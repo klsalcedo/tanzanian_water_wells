cc2d9c5f3f0ab758bb5a0133ac498565f3698016
Katarina Salcedo <ksalcedo04@gmail.com>
2021-08-10 20:50:31 -0700

    eda

--- function.py
+++ function.py
@@ -1,12 +1,13 @@
 from sklearn.metrics import accuracy_score, plot_confusion_matrix, classification_report
 
 def class_model(model, X_train, X_test, y_train, y_test):
-    #do something to normalize/scale data here
     
     model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
+    y_pred_train = model.predict(X_train)
+    y_pred_test = model.predict(X_test)
     
-    print(classification_report(y_test, y_pred))
+    print('Classification report for training data:', classification_report(y_train, y_pred_train))
+    print('Classification report for testing data:', classification_report(y_test, y_pred_test))
     
     print('Confusion Matrix Train:')
     plot_confusion_matrix(model, X_train, y_train, cmap=plt.cm.Blues)
@@ -18,7 +19,7 @@ def class_model(model, X_train, X_test, y_train, y_test):
     
     return model
 
-  #wont work for all models - use if statement or delete
+#     wont work for all models - use if statement or delete
 #     false_positive_rate, true_positive_rate, thresholds = roc_curve(y_test, y_pred)
 #     roc_auc = auc(false_positve_rate, true_positive_rate)
 #     print('AUC:', round(roc_auc,2))
