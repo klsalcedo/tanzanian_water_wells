bb3ad769a4d65966ca9ed40c6031936e9ed62c68
Katarina Salcedo <ksalcedo04@gmail.com>
2021-08-11 22:24:01 -0700

    vanilla models

--- function.py
+++ function.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import accuracy_score, plot_confusion_matrix, classification_report
+import matplotlib.pyplot as plt
 
 def class_model(model, X_train, X_test, y_train, y_test):
     
@@ -6,8 +7,11 @@ def class_model(model, X_train, X_test, y_train, y_test):
     y_pred_train = model.predict(X_train)
     y_pred_test = model.predict(X_test)
     
-    print('Classification report for training data:', classification_report(y_train, y_pred_train))
-    print('Classification report for testing data:', classification_report(y_test, y_pred_test))
+    print('Classification report for training data:')
+    print(classification_report(y_train, y_pred_train))
+          
+    print('Classification report for testing data:')
+    print(classification_report(y_test, y_pred_test))
     
     print('Confusion Matrix Train:')
     plot_confusion_matrix(model, X_train, y_train, cmap=plt.cm.Blues)
